87d1f7cbc9282e788280c04fb8a6fb3615875c8a
Tung Le <tungle251@gmail.com>
2023-01-07 11:29:02 +0700

    page responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 import BookCard from '../components/BookCard'
 import { Button } from 'semantic-ui-react'
