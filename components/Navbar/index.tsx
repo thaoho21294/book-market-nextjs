130abe590eeaa9223988ed0e103bf93b7375aba8
Tung Le <tungle251@gmail.com>
2023-01-14 20:38:52 +0700

    update nav

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -4,23 +4,19 @@ import { Icon, Image, Menu } from 'semantic-ui-react'
 import { menuItems } from '../../constants'
 import { useFetchUser } from '../../lib/user'
 import styles from '../../styles/Navbar.module.scss'
+import classNames from 'classnames'
 
 const Navbar = () => {
   const { user, loading } = useFetchUser({ required: false })
   const router = useRouter()
+  console.log(styles)
 
-  const [activeItem, setActiveItem] = useState('')
+  // const [activeItem, setActiveItem] = useState('')
   const renderMenuItem = () => {
     return menuItems.map(({ name, url }, index) => (
-      <Menu.Item
-        key={index}
-        name={name}
-        active={activeItem === name}
-        onClick={() => {
-          router.push(url)
-          setActiveItem(name)
-        }}
-      />
+      <a key={index} href={url} className={styles['navLink']}>
+        {name}
+      </a>
     ))
   }
 
@@ -29,49 +25,23 @@ const Navbar = () => {
   }
 
   return (
-    <Menu fixed="top" secondary className={styles['menu-container']}>
-      <Menu.Item>
-        <Icon name="book" size="big" circular color="green" />
-      </Menu.Item>
-      {renderMenuItem()}
-      <Menu.Menu position="right">
-        <Menu.Item>
-          <Icon name="book" />
-        </Menu.Item>
-        <Menu.Item>
-          <Icon name="search" />
-        </Menu.Item>
-        <Menu.Item>
-          <Icon name="cart" />
-        </Menu.Item>
-        {!user && (
-          <Menu.Item
-            name="login"
-            active={activeItem === 'login'}
-            onClick={() => {
-              router.push('/api/login')
-              setActiveItem('login')
-            }}
-          />
-        )}
-        {user && (
-          <>
-            <Menu.Item>
-              <Image alt="" src={user.picture} avatar />
-              <span>{user.nickname}</span>
-            </Menu.Item>
-            <Menu.Item
-              name="logout"
-              active={activeItem === 'logout'}
-              onClick={() => {
-                router.push('/api/logout')
-                setActiveItem('logout')
-              }}
-            />
-          </>
-        )}
-      </Menu.Menu>
-    </Menu>
+    <div className={styles['header-container']}>
+      <header className={styles['header']}>
+        <a href="#" className={classNames(styles['navIcon'], styles['logo'])}>
+          <Icon name="book" size="large" circular color="green"></Icon>
+        </a>
+        <div className={styles['navHeader']}>{renderMenuItem()}</div>
+        <a href="#" className={styles['navIcon']}>
+          <Icon size="large" name="search" />
+        </a>
+        <a
+          href="#"
+          className={classNames(styles['navIcon'], styles['cartButton'])}
+        >
+          <Icon size="large" name="cart"></Icon>
+        </a>
+      </header>
+    </div>
   )
 }
 
