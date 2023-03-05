cc15288a57d3f6a7a093e4c5c3008cd34752b656
Thao Ho <thaoho21294@gmail.com>
2023-03-05 10:03:46 +0700

    Add BookCard styles and apply absolute import
    - Remove unused styles in reset.css
    - Apply border radius for all images

--- components/book/BooksList/BookList.tsx
+++ components/book/BooksList/BookList.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Book } from '../../../types'
+
+import { Book } from 'types'
+
 import BookCard from '../BookCard'
 import styles from './BookList.module.css'
 
 const BookList = ({ books }: { books: Book[] }) => {
   return (
-    <div className={styles.listingContainerStyle}>
-      {books.map((b, index) => (
-        <BookCard book={b} key={b.id} eager={index === 0} />
+    <div className={styles.listingContainer}>
+      {books.map((b) => (
+        <BookCard book={b} key={b.id} />
       ))}
     </div>
   )
