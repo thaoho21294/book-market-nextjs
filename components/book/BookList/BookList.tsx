508b64b9f1d286f02b3800a5e4105ead4c1d1fa9
Thao <thaoho21294@gmail.com>
2023-03-19 12:54:40 +0700

    test

--- components/book/BookList/BookList.tsx
+++ components/book/BookList/BookList.tsx
@@ -5,6 +5,7 @@ import { Book } from 'types'
 import BookCard from '../BookCard'
 import styles from './BookList.module.css'
 
+// add a comment
 const BookList = ({ books }: { books: Book[] }) => {
   return (
     <div className={styles.listingContainer}>
