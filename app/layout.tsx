81f3c9bfbaed680b9c6b7a4ae01204da644292f8
Thao <thaoho21294@gmail.com>
2023-07-28 20:24:49 +0700

    fix(server-component): fix build error cause by 'use client' doesn't affect through default import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
 import React, { ReactNode } from 'react'
-import { Footer } from 'components/layout'
+import Footer from 'components/layout/Footer/Footer'
+import Header from 'components/layout/Header/Header'
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default async function RootLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
   return (
     <>
-      @ts-expect-error Server Component
-      {/* <Header /> */}
+      {/* @ts-expect-error Server Component*/}
+      <Header />
       <main>{children}</main>
       <Footer />
     </>
